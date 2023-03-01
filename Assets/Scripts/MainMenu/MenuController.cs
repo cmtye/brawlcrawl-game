8b23da9d858c754aa4128d2a32794ba113957709
Mitch <mcarrillo71@unm.edu>
2023-02-28 20:52:34 -0700

    Added Main Menu

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string _newGameLevel;
+
+    public void NewGameDialogYes()
+    {
+        SceneManager.LoadScene(_newGameLevel);
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+ 
+}
